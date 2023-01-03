ac884a88305839510dcbe868ee6bf8c6fac44478
HeilOdinn <odiumperfect@mail.ru>
2023-01-03 18:06:15 +0600

    lesson 4\1:19:20

--- authapp/views.py
+++ authapp/views.py
@@ -1,10 +1,30 @@
+from django.contrib import auth
 from django.shortcuts import render
+from authapp.forms import UserLoginForm
+
 
 def login(request):
-    pass
+
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = auth.authenticate(username=username, password=password)
+    else:
+        form = UserLoginForm()
+
+    context = {
+        'title': 'Geekshop | Авторизация',
+        'form' : form
+    }
+    return render(request, 'authapp/login.html', context)
 
 def register(request):
-    pass
+    context = {
+        'title': 'Geekshop | Регистрация'
+    }
+    return render(request, 'authapp/register.html', context)
 
 def logout(request):
     pass
